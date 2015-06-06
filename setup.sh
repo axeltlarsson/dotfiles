69682397200358d030adc8e85635c2a4ee80ff55
AxelTLarsson <mail@axellarsson.nu>
2015-06-06 15:27:44 +0200

    more work done on setup.sh

--- setup.sh
+++ setup.sh
@@ -1,45 +1,18 @@
 #!/bin/bash
 
 #----------------- Variables ----------------
-cd ~
-
-
-print_error() {
-    # Print output in red
-    printf "\e[0;31m  [✖] $1 $2\e[0m\n"
-}
-
-print_info() {
-    # Print output in purple
-    printf "\n\e[0;35m $1\e[0m\n\n"
-}
-
-print_question() {
-    # Print output in yellow
-    printf "\e[0;33m  [?] $1\e[0m"
-}
-
-print_result() {
-    [ $1 -eq 0 ] \
-        && print_success "$2" \
-        || print_error "$2"
-
-    [ "$3" == "true" ] && [ $1 -ne 0 ] \
-        && exit
-}
-
-print_success() {
-    # Print output in green
-    printf "\e[0;32m  [✔] $1\e[0m\n"
-}
-
+source functions
 install_zsh () {
 # Test to see if zshell is installed.  If it is:
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     # Set the default shell to zsh if it isn't currently set to zsh
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
     	print_info "Setting default shell to zsh, please enter your password"
-        chsh -s $(which zsh)
+        if [[ $(chsh -s $(which zsh)) ]]; then
+            print_success "zsh is now your shell"
+        fi
+    elif [[ $(echo $SHELL) == $(which zsh) ]]; then
+        print_info "zsh is already your shell"
     fi
 else
     # If zsh isn't installed, get the platform of the current machine
@@ -58,7 +31,4 @@ fi
 }
 
 install_zsh
-if [[ $(echo $SHELL) == $(which zsh) ]]; then
-    print_success "zsh is now your shell shell"    
-fi
-zsh 
+
