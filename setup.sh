c010424553b632c87daceeeac929322260a907b4
AxelTLarsson <mail@axellarsson.nu>
2015-06-06 14:58:12 +0200

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#----------------- Variables ----------------
+cd ~
+
+
+print_error() {
+    # Print output in red
+    printf "\e[0;31m  [✖] $1 $2\e[0m\n"
+}
+
+print_info() {
+    # Print output in purple
+    printf "\n\e[0;35m $1\e[0m\n\n"
+}
+
+print_question() {
+    # Print output in yellow
+    printf "\e[0;33m  [?] $1\e[0m"
+}
+
+print_result() {
+    [ $1 -eq 0 ] \
+        && print_success "$2" \
+        || print_error "$2"
+
+    [ "$3" == "true" ] && [ $1 -ne 0 ] \
+        && exit
+}
+
+print_success() {
+    # Print output in green
+    printf "\e[0;32m  [✔] $1\e[0m\n"
+}
+
+install_zsh () {
+# Test to see if zshell is installed.  If it is:
+if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+    # Set the default shell to zsh if it isn't currently set to zsh
+    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+    	print_info "Setting default shell to zsh"
+        chsh -s $(which zsh)
+    fi
+else
+    # If zsh isn't installed, get the platform of the current machine
+    platform=$(uname);
+    # If the platform is Linux, try an apt-get to install zsh and then recurse
+    if [[ $platform == 'Linux' ]]; then
+    	print_info "Installing zsh..."
+        sudo apt-get install zsh
+        install_zsh
+    # If the platform is OS X, tell the user to install zsh :)
+    elif [[ $platform == 'Darwin' ]]; then
+        print_error "Please install zsh, then re-run this script!"
+        exit
+    fi
+fi
+}
+
+install_zsh
+zsh 
