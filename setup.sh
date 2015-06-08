2ab6fa5462790c9ace6b7d3495b4065a77416154
AxelTLarsson <mail@axellarsson.nu>
2015-06-08 17:33:02 +0200

    symlink_prezto_files() added to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,67 @@
 #!/bin/bash
 
-#----------------- Variables ----------------
-source functions
+#----------------- utils ----------------
+answer_is_yes() {
+    [[ "$REPLY" =~ ^[Yy]$ ]] \
+        && return 0 \
+        || return 1
+}
+
+ask() {
+    print_question "$1"
+    read
+}
+
+ask_for_confirmation() {
+    print_question "$1 (y/n) "
+    read -n 1
+    printf "\n"
+}
+
+cmd_exists() {
+    [ -x "$(command -v "$1")" ] \
+        && printf 0 \
+        || printf 1
+}
+
+execute() {
+    $1 &> /dev/null
+    print_result $? "${2:-$1}"
+}
+
+get_answer() {
+    printf "$REPLY"
+}
+
+print_error() {
+    # Print output in red
+    printf "\e[0;31m  [✖] $1 $2\e[0m\n"
+}
+
+print_info() {
+    # Print output in purple
+    printf "\n\e[0;35m $1\e[0m\n\n"
+}
+
+print_question() {
+    # Print output in yellow
+    printf "\e[0;33m  [?] $1\e[0m"
+}
+
+print_result() {
+    [ $1 -eq 0 ] \
+        && print_success "$2" \
+        || print_error "$2"
+
+    [ "$3" == "true" ] && [ $1 -ne 0 ] \
+        && exit
+}
+
+print_success() {
+    # Print output in green
+    printf "\e[0;32m  [✔] $1\e[0m\n"
+}
+
 install_zsh () {
 # Test to see if zshell is installed.  If it is:
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
@@ -31,4 +91,39 @@ fi
 
 install_zsh
 
-zsh setup-prezto.zsh
+symlink_prezto_files() {
+    
+    declare -a FILES_TO_SYMLINK=$(find  runcoms -type f -not -name README.md )
+
+    local i=""
+    local sourceFile=""
+    local targetFile=""
+
+    for i in ${FILES_TO_SYMLINK[@]}; do
+
+        sourceFile="$(pwd)/$i"
+        targetFile="$HOME/$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
+
+        if [ -e "$targetFile" ]; then
+            if [ "$(readlink "$targetFile")" != "$sourceFile" ]; then
+
+                ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+                if answer_is_yes; then
+                    rm -rf "$targetFile"
+                    execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+                else
+                    print_error "$targetFile → $sourceFile"
+                fi
+
+            else
+                print_success "$targetFile → $sourceFile"
+            fi
+        else
+            execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+        fi
+
+    done
+
+}
+symlink_prezto_files
+exit 0
