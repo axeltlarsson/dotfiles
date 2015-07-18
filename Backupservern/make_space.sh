15f387e0851cf655aa61d2ca103b0a1148891df9
AxelTLarsson <mail@axellarsson.nu>
2015-07-18 19:04:18 +0200

    Some more work put in backup scripts

--- Backupservern/make_space.sh
+++ Backupservern/make_space.sh
@@ -17,7 +17,7 @@
 
 # Stores the disk usage in $usage, given file path as $1
 # Note: $1 should be a real folder path where a disk is mounted
-# such as /media/axel and NOT /dev/sda3
+# such as /media/backup and NOT /dev/sda3
 disk_usage() {
 	local free_blocks=$(stat -f --format="%a" $1)
 	local total_blocks=$(stat -f --format="%b" $1)
@@ -28,17 +28,20 @@ disk_usage() {
 # in the dir as hardcoded here in the find expression
 delete_oldest_dir() {
 	# modified from http://unix.stackexchange.com/questions/28939/how-to-delete-the-oldest-directory-in-a-given-directory
-	IFS= read -r -d $'\0' file < <(find ./test -maxdepth 1 ! -path ./test  -type d -printf '%p\0' | sort -z)
-	#rm -rf "$file"
-	find "$file" # see that it is the current file
+	IFS= read -r -d $'\0' file < <(find /media/backup -maxdepth 1 ! -path /media/backup  -type d -printf '%p\0' | sort -z)
+	echo "Deleting $file"
+	rm -rf "$file"
 }
 
-target=0.85
-disk_usage "/media/axel"
+target=0.86
+disk_usage "/media/backup"
 while [ $(echo $usage'>'$target | bc -l) == 1 ]; do
+	echo "Disk usage $usage is above $target"
 	delete_oldest_dir
-	disk_usage "/media/axel"
+	disk_usage "/media/backup"
 done
+echo "Disk usage $usage is above target $target, this script is done"
+exit 0
 
 
 
