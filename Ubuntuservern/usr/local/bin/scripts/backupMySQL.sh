1c91f94ed125464abb20081b5a93abf6021b5a0f
AxelTLarsson <mail@axellarsson.nu>
2015-10-03 18:06:14 +0200

    Add back backupMySQL.sh with sensitive info redacted

---
+++ Ubuntuservern/usr/local/bin/scripts/backupMySQL.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script för att backa upp mysql-databasen...
+# Axel Larsson 2012-01-07
+datum=`date +%Y-%m-%d_%H.%M`
+backupDir="/media/data/public/Backup/Ubuntuservern/mysqldatabases"
+mkdir -p $backupDir
+user="EDIT"
+password="EDIT"
+# Backar upp alla databaser
+mysqldump -u $user -p$password --all-databases > $backupDir/$datum.sql
+
+exit 0
