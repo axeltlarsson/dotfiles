22963e0472bcf0a7b86fdfc4ec4859848bdd6d17
AxelTLarsson <mail@axellarsson.nu>
2021-08-02 23:06:41 +0200

    wip: fzf-tab
    It works somewhat reasonably, but:
    - cannot use fzf-tmux popup
    - some cool preview options to further explore (some of it overkill ofc)

--- config/home-manager.nix
+++ config/home-manager.nix
@@ -82,26 +82,42 @@
         "utility"
         "completion"
         "fasd"
-        "fzf-tab"
         "syntax-highlighting"
         "history-substring-search"
         "prompt"
         "tmux"
+        "fzf-tab"
       ];
       pmoduleDirs = [ ../zprezto-modules ];
       extraConfig = ''
         # fzf-tab https://github.com/Aloxaf/fzf-tab
-        zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+        # zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
         # disable sort when completing `git checkout`
-        zstyle ':completion:*:git-checkout:*' sort false
         # set descriptions format to enable group support
         zstyle ':completion:*:descriptions' format '[%d]'
         # set list-colors to enable filename colorizing
         #zstyle ':completion:*' list-colors \$\{("s.:.")LS_COLORS\}
         # preview directory's content with exa when completing cd
-         zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always \$realpath'
+        zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
+        zstyle ':fzf-tab:complete:cd:*' popup-pad 30 0
         # switch group using `,` and `.`
+        # TODO: not all of this works as expected
         zstyle ':fzf-tab:*' switch-group ',' '.'
+        zstyle ':fzf-tab:complete:git-(add|diff|restore):*' fzf-preview \
+          'git diff $word | delta'|
+        zstyle ':fzf-tab:complete:git-log:*' fzf-preview \
+          'git log --color=always $word'
+        zstyle ':fzf-tab:complete:git-show:*' fzf-preview \
+          'case "$group" in
+          "commit tag") git show --color=always $word ;;
+          *) git show --color=always $word | delta ;;
+          esac'
+        zstyle ':fzf-tab:complete:git-checkout:*' fzf-preview \
+          'case "$group" in
+          "modified file") git diff $word | delta ;;
+          "recent commit object name") git show --color=always $word | delta ;;
+          *) git log --color=always $word ;;
+          esac'
       '';
 
       tmux.autoStartLocal = true;
