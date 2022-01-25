f39ab4bf9a041a6ea0d0f79f9f044a7d410f68cf
AxelTLarsson <mail@axellarsson.nu>
2022-01-25 22:07:23 +0100

    wip: Integrate nixpi into flake setup

--- flake.nix
+++ flake.nix
@@ -4,22 +4,22 @@
   inputs = {
     nixpkgs.url = "flake:nixpkgs";
     # TODO use "github:nixos/nixpkgs/nixos-unstable"?
-    homeManager = {
+    home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixkpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, homeManager }: {
+  outputs = { self, nixpkgs, home-manager }: {
     homeConfigurations = {
-      "axel_mbp16" = homeManager.lib.homeManagerConfiguration {
+      "axel_mbp16" = home-manager.lib.homeManagerConfiguration {
         configuration = import ./axel_mbp16.nix;
         system = "x86_64-darwin";
         homeDirectory = "/Users/axel";
         username = "axel";
         stateVersion = "21.11";
       };
-      "andrimner" = homeManager.lib.homeManagerConfiguration {
+      "andrimner" = home-manager.lib.homeManagerConfiguration {
         configuration = import ./andrimner.nix;
         system = "x86_64-linux";
         homeDirectory = "/home/axel";
@@ -27,6 +27,22 @@
         stateVersion = "21.11";
       };
     };
+    nixosConfigurations = {
+      nixpi = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          ./nixpi/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.axel = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+        ];
+      };
+    };
   };
-  # TODO: nixpi nixos configuration (including home-manager conf)
 }
