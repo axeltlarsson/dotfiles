d00d4126ca45a82a083299e727e10f1802eddd78
AxelTLarsson <mail@axellarsson.nu>
2022-03-16 19:02:16 +0100

    Update nixpi configuration using nixos-hardware

--- nixpi/configuration.nix
+++ nixpi/configuration.nix
@@ -2,14 +2,19 @@
 
 {
   imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
+    # Use Nixos/nixos-hardware to configure the RPI 4 hw
+    "${
+      builtins.fetchGit { url = "https://github.com/NixOS/nixos-hardware.git"; }
+    }/raspberry-pi/4"
   ];
 
-  # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
-  boot.loader.grub.enable = false;
-  # Enables the generation of /boot/extlinux/extlinux.conf
-  boot.loader.generic-extlinux-compatible.enable = true;
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
+  };
 
   # Enable OpenSSH out of the box.
   services.sshd.enable = true;
@@ -18,15 +23,20 @@
 
   system.autoUpgrade.enable = true;
 
-  users.users = {
-    axel = {
-      isNormalUser = true;
-      extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-      home = "/home/axel";
-      description = "Axel Larsson";
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID8afXGSxXnnz5ydf/AHGH65b2SpHvd1bEE6Q5JASQIM axel@axel-mbp16 "
-      ];
+  users = {
+    mutableUsers = false;
+    users = {
+      axel = {
+        shell = pkgs.zsh;
+        isNormalUser = true;
+        extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+        home = "/home/axel";
+        password = "duh"; # or initialHashedPassword?
+        description = "Axel Larsson";
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID8afXGSxXnnz5ydf/AHGH65b2SpHvd1bEE6Q5JASQIM axel@axel-mbp16 "
+        ];
+      };
     };
   };
 
@@ -65,24 +75,6 @@
       max-free = ${toString (1024 * 1024 * 1024)}
     '';
   };
-
-  # Use a default root SSH login.
-  # services.openssh.permitRootLogin = "yes";
-  # users.users.root.password = "nixos";
-
-  # Wireless networking (1). You might want to enable this if your Pi is not attached via Ethernet.
-  # networking.wireless = {
-  #  enable = true;
-  #  interfaces = [ "wlan0" ];
-  #  networks = {
-  #    "replace-with-my-wifi-ssid" = {
-  #      psk = "replace-with-my-wifi-password";
-  #    };
-  #  };
-  # };
-
-  # Wireless networking (2). Enables `wpa_supplicant` on boot.
-  # systemd.services.wpa_supplicant.wantedBy = lib.mkOverride 10 [ "default.target" ];
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
