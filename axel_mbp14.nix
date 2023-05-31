21cbc2e1f7da5f313f155baab41474c2f941dc8b
AxelTLarsson <mail@axellarsson.nu>
2023-05-31 20:21:32 +0200

    🔑 Add new gpg key

--- axel_mbp14.nix
+++ axel_mbp14.nix
@@ -1,6 +1,6 @@
 # mbp14-specific configuration goes here, see mbp.nix for common macOS configuration
 { config, pkgs, ... }: {
   imports = [ ./mbp.nix ];
-  programs.git.signing = { key = "962B43506E2DAB3C"; };
+  programs.git.signing = { key = "3AE85E14F3123D07"; };
   programs.git.userEmail = "mail@axellarsson.nu";
 }
