7aa8fd494aee9ceec30605db4372cc5cd8fc77ae
AxelTLarsson <mail@axellarsson.nu>
2024-01-21 19:00:07 +0100

    🔑 Set up gpg agent and key

--- nixpi/nixpi.nix
+++ nixpi/nixpi.nix
@@ -2,12 +2,29 @@
 { config, pkgs, ... }: {
   imports = [ ../config/home.nix ];
 
-  programs.git.signing = { key = "dummy"; };
+  programs.git.signing = { key = "89006B84F7EC0084"; };
   programs.git.userEmail = "mail@axellarsson.nu";
 
   home = {
     username = "axel";
     homeDirectory = "/home/axel";
     stateVersion = "24.05";
+  };
+
+  home.packages = [
+    pkgs.pinentry-curses
+  ];
+
+
+  programs = {
+    gpg = { enable = true; };
+  };
+
+  services.gpg-agent = {
+    enable = true;
+    enableZshIntegration = true;
+    extraConfig = ''
+      pinentry-program ${pkgs.pinentry-curses}/bin/pinentry-curses
+    '';
   };
 }
