046c6eb927ae46ac4f4ecd964b05fe067f9f124e
axeltlarsson <axel@arthro.ai>
2025-02-25 10:58:14 +0100

    🔧 Configure git with better options
    https://blog.gitbutler.com/how-git-core-devs-configure-git/

--- config/git.nix
+++ config/git.nix
@@ -1,11 +1,14 @@
-{ pkgs, config, ... }: {
+{ pkgs, config, ... }:
+{
   programs.git = {
     enable = true;
     userName = "axeltlarsson";
 
     delta = {
       enable = true;
-      options = { };
+      options = {
+        navigate = true;
+      };
     };
 
     aliases = {
@@ -21,16 +24,52 @@
         excludesFile = "~/.gitignore"; # TODO: inline?
       };
 
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+      init.defaultBranch = "main";
+
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+
       push = {
         default = "simple";
         helper = "cache --timeout=18000";
         autoSetupRemote = true;
+        followTags = true;
+      };
+
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
+
+      grep.patternType = "perl";
+
+      help.autocorrect = "prompt";
+
+      commit.verbose = true;
+
+      rerere = {
+        enabled = true;
+        autoupdate = true;
+      };
+
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRef = true;
       };
 
       pull.rebase = false;
       merge = {
         tool = "nvim";
-        conflictstyle = "diff3";
+        conflictstyle = "zdiff3";
       };
       mergeTool.keepBackup = false;
       mergeTool = {
@@ -46,6 +85,8 @@
 
     lfs.enable = true;
 
-    signing = { signByDefault = true; };
+    signing = {
+      signByDefault = true;
+    };
   };
 }
