db41c0bd083e3a734252fa699110aadd918d136b
axeltlarsson <axel@arthro.ai>
2025-12-18 13:57:55 +0100

    Configure fzf to use fd, and show bat preview windows

--- config/fzf.nix
+++ config/fzf.nix
@@ -1,39 +1,51 @@
 {
   programs.fzf = {
+    # N.B! `tmux kill-server` after changing — tmux caches env vars int is global server environment
     enable = true;
     enableZshIntegration = true;
-    defaultCommand = "rg --files --hidden --follow --glob '!.git/*'";
-    defaultOptions = let
-      theme = import ./rose-pine.nix;
-      # fzf COLOR NAMES:
-      # fg         Text
-      # bg         Background
-      # preview-fg Preview window text
-      # preview-bg Preview window background
-      # hl         Highlighted substrings
-      # fg+        Text (current line)
-      # bg+        Background (current line)
-      # gutter     Gutter on the left (defaults to bg+)
-      # hl+        Highlighted substrings (current line)
-      # query      Query string
-      # disabled   Query string when search is disabled
-      # info       Info line (match counters)
-      # border     Border around the window (--border and --preview)
-      # prompt     Prompt
-      # pointer    Pointer to the current line
-      # marker     Multi-select marker
-      # spinner    Streaming input indicator
-      # header     Header
-    in with theme; [
-      "--height 40%"
-      "--border"
-      "--color=fg:${text},bg:${surface}"
-      "--color=hl:${muted},fg+:${subtle},bg+:${base}"
-      "--color=hl+:${subtle}"
-      "--color=query:${gold}"
-      "--color=spinner:${love},header:${rose}"
-      "--color=info:${foam},pointer:${iris}"
-      "--color=marker:${rose},prompt:${gold}"
+    defaultCommand = "fd --type f";
+    defaultOptions =
+      let
+        theme = import ./rose-pine.nix;
+        # fzf COLOR NAMES:
+        # fg         Text
+        # bg         Background
+        # preview-fg Preview window text
+        # preview-bg Preview window background
+        # hl         Highlighted substrings
+        # fg+        Text (current line)
+        # bg+        Background (current line)
+        # gutter     Gutter on the left (defaults to bg+)
+        # hl+        Highlighted substrings (current line)
+        # query      Query string
+        # disabled   Query string when search is disabled
+        # info       Info line (match counters)
+        # border     Border around the window (--border and --preview)
+        # prompt     Prompt
+        # pointer    Pointer to the current line
+        # marker     Multi-select marker
+        # spinner    Streaming input indicator
+        # header     Header
+      in
+      with theme;
+      [
+        "--height 40%"
+        "--border"
+        "--color=fg:${text},bg:${surface}"
+        "--color=hl:${muted},fg+:${subtle},bg+:${base}"
+        "--color=hl+:${subtle}"
+        "--color=query:${gold}"
+        "--color=spinner:${love},header:${rose}"
+        "--color=info:${foam},pointer:${iris}"
+        "--color=marker:${rose},prompt:${gold}"
+      ];
+
+    # FileWidget Ctrl-T
+    fileWidgetCommand = "fd --type f";
+    fileWidgetOptions = [
+      "--preview 'bat --style=numbers --color=always --line-range :500 {}'"
+      "--layout=reverse"
+      "--preview-window=right:60%"
     ];
 
     tmux.enableShellIntegration = true;
