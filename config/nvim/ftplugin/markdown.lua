d25dc4cd5f3f13b01444426080316559121af1e0
axeltlarsson <axel@arthro.ai>
2026-01-08 09:05:36 +0100

    ✔️ Add toggle_todo function for markdown todos

--- config/nvim/ftplugin/markdown.lua
+++ config/nvim/ftplugin/markdown.lua
@@ -4,3 +4,39 @@ end, {
   buffer = true,
   desc = "Insert note link",
 })
+
+local function toggle_todo_line(line)
+  if line:match("^(%s*)[%-%*] %[ %]") then
+    return (line:gsub("(%[) (%])", "%1x%2", 1))
+  elseif line:match("^(%s*)[%-%*] %[x%]") then
+    return (line:gsub("(%[)x(%])", "%1 %2", 1))
+  end
+  return nil
+end
+
+vim.keymap.set("n", "<leader>tt", function()
+  local new_line = toggle_todo_line(vim.api.nvim_get_current_line())
+  if new_line then
+    vim.api.nvim_set_current_line(new_line)
+  end
+end, {
+  buffer = true,
+  desc = "Toggle markdown todo",
+})
+
+vim.keymap.set("x", "<leader>tt", function()
+  local start_line = vim.fn.line("v")
+  local end_line = vim.api.nvim_win_get_cursor(0)[1]
+  if start_line > end_line then
+    start_line, end_line = end_line, start_line
+  end
+
+  local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
+  for i, line in ipairs(lines) do
+    lines[i] = toggle_todo_line(line) or line
+  end
+  vim.api.nvim_buf_set_lines(0, start_line - 1, end_line, false, lines)
+end, {
+  buffer = true,
+  desc = "Toggle markdown todos",
+})
