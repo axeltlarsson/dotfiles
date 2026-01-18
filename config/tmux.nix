98420947f506d1f6ad14bddcafa59fa0960532e4
axeltlarsson <mail@axellarsson.nu>
2026-01-18 15:12:47 +0100

    ✨ Bind u in tmux to a popup terminal

--- config/tmux.nix
+++ config/tmux.nix
@@ -19,8 +19,12 @@
       # prefix + t swaps window to "top"
       bind-key t swap-window -t 0 -d
 
-      # prefix + b swaps to last window
+      # prefix + b jumps to last window
       bind-key b last-window
+
+      # prefix + u shows popup terminal
+      bind-key u display-popup -E "$SHELL"
+
 
       # https://github.com/tmux/tmux/issues/4240
       set -gu default-command
