aa2508b3a387cadb6c5abaeffc02d1be9f382179
axeltlarsson <axel@arthro.ai>
2026-04-08 10:59:49 +0200

    ⬆️ Update tmux rose-pine plugin to 2026-03-24

--- overlays/tmux-plugins.nix
+++ overlays/tmux-plugins.nix
@@ -1,12 +1,12 @@
 final: prev: {
   tmuxPlugins = prev.tmuxPlugins // {
     rose-pine = prev.tmuxPlugins.rose-pine.overrideAttrs (old: {
-      version = "0-unstable-2026-01-02";
+      version = "0-unstable-2026-03-24";
       src = prev.fetchFromGitHub {
         owner = "rose-pine";
         repo = "tmux";
-        rev = "8613447e75a2acead1cf2947e01991574b6371b9";
-        hash = "sha256-Nd84SLB2UvqA/qYou8YSrqvt8/RWhJZgU9Nfw/nwxEA=";
+        rev = "b6138c51573425ccdc33c91464597323baec3b7e";
+        hash = "sha256-HDmCCRhTCPfu7gL9VPHVGCiG5IcnkpQ4EaXN4IsQ0YE=";
       };
     });
   };
